61f4ac3fc16c2cfef30a7b6e3210ade3129c70cb
John Urdaneta <johnadrian.urdaneta@dcmail.ca>
2018-03-01 14:08:21 -0500

    First lab
    - Not functioning because of array
    - Will fix soon
    - Everything else is functioning

---
+++ AverageUnitShipped/AverageShipped/AverageShipped.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAverageShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lbUnits = New System.Windows.Forms.Label()
+        Me.lbDays = New System.Windows.Forms.Label()
+        Me.lbDisplayInput = New System.Windows.Forms.Label()
+        Me.lbDisplayAverage = New System.Windows.Forms.Label()
+        Me.tbUserInput = New System.Windows.Forms.TextBox()
+        Me.lbNumberOfDays = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(17, 272)
+        Me.btnEnter.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(100, 28)
+        Me.btnEnter.TabIndex = 0
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(140, 272)
+        Me.btnReset.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(100, 28)
+        Me.btnReset.TabIndex = 1
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(263, 272)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(100, 28)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lbUnits
+        '
+        Me.lbUnits.AutoSize = True
+        Me.lbUnits.Location = New System.Drawing.Point(68, 38)
+        Me.lbUnits.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbUnits.Name = "lbUnits"
+        Me.lbUnits.Size = New System.Drawing.Size(48, 17)
+        Me.lbUnits.TabIndex = 3
+        Me.lbUnits.Text = "Units: "
+        '
+        'lbDays
+        '
+        Me.lbDays.AutoSize = True
+        Me.lbDays.Location = New System.Drawing.Point(239, 38)
+        Me.lbDays.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbDays.Name = "lbDays"
+        Me.lbDays.Size = New System.Drawing.Size(33, 17)
+        Me.lbDays.TabIndex = 4
+        Me.lbDays.Text = "Day"
+        '
+        'lbDisplayInput
+        '
+        Me.lbDisplayInput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbDisplayInput.Location = New System.Drawing.Point(68, 70)
+        Me.lbDisplayInput.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbDisplayInput.Name = "lbDisplayInput"
+        Me.lbDisplayInput.Size = New System.Drawing.Size(225, 167)
+        Me.lbDisplayInput.TabIndex = 5
+        '
+        'lbDisplayAverage
+        '
+        Me.lbDisplayAverage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbDisplayAverage.Location = New System.Drawing.Point(68, 239)
+        Me.lbDisplayAverage.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbDisplayAverage.Name = "lbDisplayAverage"
+        Me.lbDisplayAverage.Size = New System.Drawing.Size(225, 30)
+        Me.lbDisplayAverage.TabIndex = 6
+        '
+        'tbUserInput
+        '
+        Me.tbUserInput.Location = New System.Drawing.Point(140, 34)
+        Me.tbUserInput.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.tbUserInput.Name = "tbUserInput"
+        Me.tbUserInput.Size = New System.Drawing.Size(99, 22)
+        Me.tbUserInput.TabIndex = 7
+        '
+        'lbNumberOfDays
+        '
+        Me.lbNumberOfDays.AutoSize = True
+        Me.lbNumberOfDays.Location = New System.Drawing.Point(273, 38)
+        Me.lbNumberOfDays.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbNumberOfDays.Name = "lbNumberOfDays"
+        Me.lbNumberOfDays.Size = New System.Drawing.Size(0, 17)
+        Me.lbNumberOfDays.TabIndex = 9
+        '
+        'frmAverageShipped
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(379, 321)
+        Me.Controls.Add(Me.lbNumberOfDays)
+        Me.Controls.Add(Me.tbUserInput)
+        Me.Controls.Add(Me.lbDisplayAverage)
+        Me.Controls.Add(Me.lbDisplayInput)
+        Me.Controls.Add(Me.lbDays)
+        Me.Controls.Add(Me.lbUnits)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmAverageShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lbUnits As Label
+    Friend WithEvents lbDays As Label
+    Friend WithEvents lbDisplayInput As Label
+    Friend WithEvents lbDisplayAverage As Label
+    Friend WithEvents tbUserInput As TextBox
+    Friend WithEvents lbNumberOfDays As Label
+End Class
---
+++ AverageUnitShipped/AverageShipped/AverageShipped.vb
@@ -0,0 +1,103 @@
+﻿'Option Strict On
+''' <summary>
+''' Author:         John Urdaneta
+''' Project Name:   AverageShipped
+''' Date:           02-02-18
+''' Description:    Demonstrate the understanding in chapters 1,2,3,4,5 and 8.
+'''                 take an input for each day in the week numbered from 1 to 7,
+'''                 Then calculates the average for the input in those 7 days.
+''' </summary>
+
+
+Public Class frmAverageShipped
+    ' Declare the number of days as long as the form/class lives.
+    Private numberOfDays As Integer = 0 ' Governs the number of days in a week
+    ''' <summary>
+    ''' btnEnter_Click - enters users input and calls the function 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+        'Variables
+        'Dim numberOne As Double = 2 
+        'Dim numberTwo As Double = 5
+        Dim daysInAWeek As Integer = 1
+
+        'numberOfDays += Add(numberOne, numberTwo)
+        Dim convertedValue As Integer = 0
+        Dim minimumInput As Integer = 0
+        Dim maximumOuput As Integer = 1000
+        Dim userInput As Integer                     'holds the user input as a string
+        Dim userInputDouble As Double = 0.0          'holds the user input to convert to double just incase
+        'Dim userInputInteger As Integer = 0         'holds the user input to convert to integer just incase
+        'Dim output As String = String.Empty         'holds the output to be displayed to the user
+        'Dim showDays(7) As Integer                  'holds the number of days in a week
+        'Const minimumarraysize As Integer = 1
+        'If String.IsNullOrWhiteSpace(userInput) Then
+        '    Throw New ArgumentException("Input cannot be blank! ")
+        'End If
+
+        For numberOfDays As Integer = 1 To daysInAWeek
+            lbNumberOfDays.Text = numberOfDays.ToString
+        Next
+
+        userInput = Integer.Parse(tbUserInput.Text)
+        lbDisplayInput.Text = userInput
+        'If Integer.TryParse(userInput, convertedValue) Then
+        '    If convertedValue < minimumInput OrElse convertedValue >= maximumOuput Then
+        '        MsgBox("Invalid entry, please try agian!", userInput)
+        '    End If
+        'End If
+        '    userinput = tbUserInput.Text                'get the input from the textbox
+
+        '    If (Double.TryParse(userinput, userinputdouble)) Then           ' if the input is a double
+
+        '        If (Integer.TryParse(userinput, userinputinteger) AndAlso   ' if the input is an integer
+        '        userinputinteger >= minimumarraysize) Then              ' also, there must be at least 1 element
+        '            processing
+        '        End If
+        '    End If
+    End Sub
+
+    ''' <summary>
+    ''' Add -  Adds two numbers 
+    '''     *** Function is a self contained area of code that contains one or more lines of code that can be called by another procedure and returns a value to the calling code.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <returns>Double - the sum of the two arguments passed to the Method/Function</returns>
+    'Private Function Add(numberOne As Double, numberTwo As double) As Double
+    '    ' Declare a return value variable
+    '    Dim returnValue As Double 
+
+    '    ' Add number one parameter to number two parameter
+    '    ' And assign it to return value
+    '    returnValue = numberOne + numberTwo
+
+    '    ' Return the return value variable
+    '    Return returnValue
+    'End Function
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+    Private Sub BtnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        ' This resets the textbox and output.
+        tbUserInput.ResetText()
+        lbDisplayInput.ResetText()
+        lbDisplayAverage.ResetText()
+
+        ' Resets the number of days
+        lbNumberOfDays.ResetText()
+    End Sub
+
+    Private Sub tbUserInput_TextChanged(sender As Object, e As EventArgs) Handles tbUserInput.TextChanged
+
+    End Sub
+
+End Class
